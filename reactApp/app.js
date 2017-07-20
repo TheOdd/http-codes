610ff537b3397fa16f8537435bb8e44a09778b54
Owen Hines <owenhines07@gmail.com>
2017-07-19 19:21:34 -0700

    Set up Babel, React, and Webpack with minimal content.

---
+++ reactApp/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CodesApp from './components/CodesApp';
+
+ReactDOM.render(<CodesApp />, document.getElementById('root'))
---
+++ reactApp/components/CodesApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CodesApp extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <h1>Placeholder</h1>
+    )
+  }
+}
+
+export default CodesApp;
