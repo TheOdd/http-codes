b2b9d336fe139a70df0ed975a7fa598eb6384e98
Owen Hines <owenhines07@gmail.com>
2017-09-13 13:45:11 -0500

    Added 'additional info' buttons w/ working modals.

--- frontend/components/CodesList.js
+++ frontend/components/CodesList.js
@@ -2,17 +2,60 @@ import React from 'react';
 import codes from '../codes.json';
 import uuidv1 from 'uuid/v1';
 import PropTypes from 'prop-types';
+import { Modal } from 'react-bootstrap';
 
-const CodesList = ( { text } ) => {
-  return (
-      <ul style={{marginLeft: '35%'}}>
-        {codes.map(code => {
-          if (code.number.toString().indexOf(text) != -1) {
-            return <li key={uuidv1()}>{code.number} - {code.resp}</li>
-          }
-      })}
-      </ul>
-  )
+class CodesList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false,
+      currentCode: null
+    }
+  }
+
+  openModal(code) {
+    this.setState({
+      showModal: true,
+      currentCode: code
+    })
+  }
+
+  closeModal() {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  render() {
+    const text = this.props.text;
+    return (
+      <div>
+        <ul style={{marginLeft: '35%'}}>
+          {codes.map(code => {
+            if (code.number.toString().indexOf(text) != -1) {
+              return (
+                <li key={uuidv1()}>
+                  {code.number} - {code.resp} <i style={{fontSize: '0.55em'}} className="glyphicon glyphicon-info-sign" onClick={() => this.openModal(code)}></i>
+                </li>
+              )
+            }
+        })}
+        </ul>
+        <Modal show={this.state.showModal} onHide={() => this.closeModal()}>
+          {this.state.showModal ? (
+            <div>
+              <Modal.Header closeButton>
+                <Modal.Title>{this.state.currentCode.number} - {this.state.currentCode.resp}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p>{this.state.currentCode.desc}</p>
+              </Modal.Body>
+            </div>
+          ) : null}
+        </Modal>
+      </div>
+    )
+  }
 }
 
 CodesList.propTypes = {
