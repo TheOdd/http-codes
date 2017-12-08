f8f507531e0eec6219c0d5bd48e6f18cf40fb4ee
TheOdd <owenhines07@gmail.com>
2017-12-08 10:15:20 -0600

    Typo fix

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
   render () {
     return (
       <div className="footer-wrapper">
-        <p>This project was made by <a href='https://github.com/TheOdd'>TheOdd</a> and it's source code is freely available at the <a href='https://github.com/TheOdd/http-codes/'>HTTP Codes respoitory</a>.</p>
+        <p>This project was made by <a href='https://github.com/TheOdd'>TheOdd</a> and its source code is freely available at the <a href='https://github.com/TheOdd/http-codes/'>HTTP Codes respoitory</a>.</p>
       </div>
     )
   }
